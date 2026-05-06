e1fb44f71ccc460cde64ae7075d2f2f3141d499a
MaryamJami1 <maryamchouhan373@gmail.com>
2026-05-06 13:20:16 +0500

    data types

--- javascript/java/test.js
+++ javascript/java/test.js
@@ -72,6 +72,79 @@ console.log(firstName,last_name);
 
 
 
+// data types
+
+
+
+// primitive data types
+// immutable
+
+// string (text)
+/*let name1 = "Maryam"
+
+
+console.log(typeof(name1));
+
+// number
+let num = 30;
+let price = 99.99
+console.log(typeof(price));
+
+// boolean
+let isPass = true
+let isFail = false
+
+console.log(typeof(isFail));
+
+let color;
+console.log(color);
+console.log(typeof(color));
+
+
+let isHappy = null;
+console.log(isHappy);
+console.log(typeof(isHappy));
+
+
+
+
+let integer = BigInt(233444566779753456);
+console.log(integer);
+console.log(typeof(integer));
+
+
+let id = Symbol(60)
+console.log(id);
+console.log(typeof(id));*/
+
+
+
+
+
+// let name = "Jamil"
+// console.log(name);
+
+// name = 89
+// console.log(name);
+
+
+
+let a = 20;
+console.log(a);
+
+
+// copy
+let b = a   //20
+console.log(b);
+
+
+a = 30;
+console.log(a);
+console.log(b);
+
+
+
+
 
 
 
